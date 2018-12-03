ef47915db2c8d596864f270fbc080e0dea31b738
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-12-02 20:52:53 -0800

    Removed sidebar.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,17 +2,11 @@ module.exports = {
   title: 'My Vuepress Blog',
   description: 'This is my Vuepress experimental test blog to learn the framework.',
   themeConfig: {
-    docsDir: 'docs',
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Blog', link: '/blog' },
       { text: 'Contact', link: '/contact' },
       { text: 'Admin', link: '/admin/#/' }
-    ],
-    sidebar: [
-      '/',
-      '/blog',
-      '/contact'
     ]
   }
 }
